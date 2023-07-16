6a6524ece69666ab6a800c486f73e8372f27e0e8
unknown <elmaximolider75@gmail.com>
2023-07-16 17:37:09 -0500

    arrgelo del bg-color

--- src/components/Design.vue
+++ src/components/Design.vue
@@ -7,9 +7,9 @@ const model = reactive(data);
 </script>
 
 <template>
-  <div class="flex items-center justify-center h-screen bg-red-200">
+  <div class="flex items-center justify-center h-screen bg-slate-100">
     <div
-      class="flex flex-col md:flex-row h-full md:h-auto md:w-2/5 bg-white rounded-xl"
+      class="flex flex-col md:flex-row h-full md:h-auto md:w-2/5 bg-white rounded-xl shadow-xl"
     >
       <div
         class="flex flex-col pt-6 items-center bg-[#5A3DF9] rounded-b-3xl md:rounded-xl pb-8 text-slate-300 text-center w-full"
@@ -17,10 +17,10 @@ const model = reactive(data);
         <h1 class="pb-5 text-lg">Your Result</h1>
 
         <div
-          class="flex flex-col justify-center bg-[#3A2FED] w-32 h-32 md:w-32 md:h-32 rounded-full bg-opacity-80 "
+          class="flex flex-col justify-center bg-[#3A2FED] w-32 h-32 md:w-32 md:h-32 rounded-full bg-opacity-80 shadow-2xl"
         >
           <span class="text-white text-5xl font-bold">76 </span>
-          <span class="pt-2 text-xs opacity-50  ">of 100 </span>
+          <span class="pt-2 text-xs opacity-50">of 100 </span>
         </div>
 
         <div class="flex flex-col px-20 md:px-11 pt-5">
@@ -42,7 +42,7 @@ const model = reactive(data);
 
           <input
             v-model="index.category"
-            class=" bg-opacity-5 font-bold absolute w-full px-10 py-2 pr-14 appearance-none rounded-md border border-gray-300 shadow-sm placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            class="bg-opacity-5 font-bold absolute w-full px-10 py-2 pr-14 appearance-none rounded-md border border-gray-300 shadow-sm placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             :class="index.textColor"
           />
           <span class="text-xs mr-2">
